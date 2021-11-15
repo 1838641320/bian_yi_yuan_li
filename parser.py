a3040547f95952286ba253f460832a1011c23bf2
1838641320 <60167568+1838641320@users.noreply.github.com>
2021-11-15 20:31:41 +0800

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,23 +1,145 @@
 import sys
 
-grammer=set()
+grammer=list()
 first=dict()
 follow=dict()
 select=dict()
 catergory_dict=set()
 
-def read_grammer_file(file):
+def read_terminal(file):# 输入终结符
+    global catergory_dict
+    f=open(file,"r",encoding="UTF-8")
+    catergory_dict=f.readlines()
+    f.close()
+
+def read_grammer_file(file):# 输入文法
     global grammer
     f=open(file,"r",encoding="UTF-8")
-    grammer=f.readlines()
+    file_content=f.readlines()
     f.close()
-    line_count=0
-    for line in grammer:
-        grammer=line.splite()
-        grammer[line_count].remove('::=')
-        line_count+=1
+    for line in file_content:
+        temp=line.splite()# 空格分隔
+        temp.remove("::=")
+        grammer.append(temp)
+    
 
-def isTerminal(str):
-    return str in catergory_dict
+def isTerminal(str):return str in catergory_dict
 def isKong(str:str):return str=="eps"
 
+def getFirst():# 课本P78
+    global first
+    first=dict()
+    for line in grammer:
+        for token in line: # 首先加入终结符
+            first[token]=set()
+            if isTerminal(token) or isKong(token):
+                first[token].add(token)
+    for cnt in range(len(grammer)+9):# bellman_ford
+        update=0
+        for line in grammer:
+            lens=len(first[line[0]])
+            addt=line[0] # 产生式行开始的终结符
+            i=1 # 记录该产生式第几个符号
+            for token in line[1:]:
+                i+=1
+                # 产生式无空或者first集有空
+                if ("eps" not in first[token]) or i==len(line):
+                    first[addt].union(first[token])
+                    break
+
+                t=first[token].copy()
+                t.remove("eps")
+                first[addt].union(t)
+
+            if lens != len(first[addt]): # 有更新
+                update=1
+        if not update:break # 无更新,break
+
+
+def getFollow():# 课本P79
+    global follow
+    follow = {}
+    for line in grammer:
+        for token in line:
+            if not isTerminal(token) and token != "eps":
+                follow[token]=set() # 只有非终结符才有follow集
+    follow['program']+=["#"]
+
+    while(1):
+        update=0
+        for line in grammer:
+            for i in range(1,len(line)):
+                if isTerminal(i) or i == 'eps':
+                    continue
+                lens=len(follow[line[i]])
+                fir=set()
+                for j in range(i+1,len(line)):# 获取后面的first集
+                    if ("eps" not in first[line[j]]) or (j==len(line)-1):
+                        fir.union(first[line[j]])
+                        break
+                    t=first[line[j]].copy()
+                    t.remove("eps")
+                    fir.union(t)
+
+                if ("eps" in fir) or len(fir)==0:
+                    fir.union(follow[line[0]])
+                    fir.add("eps")
+                    fir.remove("eps") # 去除空
+                follow[line[i]].union(fir)
+                if lens!=len(follow[line[i]]):
+                    update=1
+
+        if not update:break
+
+
+def getSelect():
+    global select
+    select =dict()
+    for i in range(len(grammer)):
+        line=grammer[i]
+        j=0
+        fir=set()
+        for j in range(1,len(line)):# 获取产生式后面的first集
+            if ("eps" not in first[line[j]]) or (j==len(line)-1):
+                fir.union(first[line[j]])
+                break
+            t=first[line[j]].copy()
+            t.remove("eps")
+            fir.union(t)
+        if ("eps" in fir):# 如果右边产生式可以为空
+            fir.union(follow[line[0]])# 加入终结符的follow集
+        select[i]=fir
+
+terminal=set()
+nterminal=set()
+LL_1Table=dict() # 字典里面的终结符映射到产生式 str -> int
+def getLL_1Table():
+    global terminal,nterminal
+    for line in grammer:
+        for token in line:
+            if isTerminal(token) or token=="eps":
+                terminal.add(token)
+            else: nterminal.add(token)
+    terminal.add("#")
+    for nt in nterminal:
+        LL_1Table[nt]=dict()
+    for line in select:
+        nt=grammer[line][0]
+        for tem in select[line]:
+            if tem not in LL_1Table[nt]:
+                LL_1Table[nt][tem]=line
+            else:
+                print("文法错误")
+        
+
+def analysis():
+    1
+
+if __name__ == "__main__":
+    file_name="grammer.txt"
+    read_grammer_file(file_name)
+    getFirst()
+    getFollow()
+    getSelect()
+    getLL_1Table()
+    analysis()
