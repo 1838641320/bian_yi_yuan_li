4eb9ecd3e838d5fe872af2045426c2e741312fc4
1838641320 <60167568+1838641320@users.noreply.github.com>
2022-01-16 19:38:51 +0800

    课设文件

---
+++ 课设/lex.py
@@ -0,0 +1,117 @@
+import sys
+import re
+
+MAXL=80
+
+operator=[
+	"+","-","*","/","<","<=",">",">=","=","==",
+   "!=",";","(",")","^",",","\"","\'","#","&",
+   "&&","|","||","%","~","<<",">>","[","]","{",
+   "}","\\",".","?",":","!","->","++","--"]
+
+key_words=[
+	"auto","break","case","char","const","continue","default",
+	"do","double","else","enum","extern","float","for","goto","if",
+	"inline","int","long","register","restrict","return","short",
+	"signed","sizeof","static","struct","switch","typedef","union",
+	"unsigned","void","volatile","while"]
+
+def is_blank(s:str):
+	if(len(s)==0):return 0
+	return s.isspace()
+
+def find_op_or_key_words(s:str)->tuple:
+	def is_break(s:str)->bool:
+		if(len(s)==0):return 1
+		if(s.isalnum()or s=='_'): return 0
+		return 1
+	mat=''
+	ty=''
+	for i in operator:
+		if(s[:len(i)]==i and len(i)>len(mat)):
+			ty,mat="operator",i
+	for i in key_words:
+		li=len(i)
+		if(s[:li]==i and li>len(mat) and is_break(s[li:li+1])):
+			ty,mat="key_words",i
+	if(len(mat)==0):return (0,0)
+	return (mat,ty)
+
+def find_number(s:str)->tuple:
+	integer=re.search(r"^[-+]?([1-9][0-9]*|0)",s)
+	floating=re.search(r"^[-+]?([1-9][0-9]*|0?)\.?[0-9]*",s)
+	if(integer==None):return (0,)
+	seg=floating.span()[1]
+	ty='float_constant' if floating.span()[1]>integer.span()[1] else 'integer'
+	return (ty,s[0:seg])
+
+def find_string_or_char(s:str)->tuple:
+	res=re.search(r'^"[^"]*"',s)
+	if(res!=None):return ("string",s[0:res.span()[1]])
+	res=re.search(r"'[^']'",s)
+	if(res!=None):return ("integer",ord(s[1]))
+	return (0,0)
+
+def find_identifier(s:str)->tuple:
+	res=re.search(r"^[_a-z][_a-zA-Z0-9]*",s)
+	if(res==None):return (0,0)
+	return ("ID",s[0:res.span()[1]])
+
+def remove_line_note(s:str):
+	s=list(s)
+	sta=0
+	for i in range(len(s)):
+		if(s[i]=='/'):sta+=1
+		if(s[i]=='\n'):sta=0
+		if(sta>=2):
+			s[i]=' '
+			if(s[i-1]=='/'):s[i-1]=' '
+	return ''.join(s)
+
+def main()->list:
+	fin =open("code.txt","r",encoding="UTF-8")
+	fout=open("Lexical_analysis_result.txt","w",encoding="UTF-8")
+	intp=fin.readlines()
+	for i in range(1,len(intp)):intp[0]+=intp[i]
+	intp=remove_line_note(intp[0])
+	maxl=len(intp)
+	ptr=0
+	result=[]
+	while(ptr<maxl):
+		while(ptr<maxl and is_blank(intp[ptr])):ptr+=1
+		if(ptr>=maxl):break
+		nexts=intp[ptr:ptr+MAXL]
+		tp=find_string_or_char(nexts)
+		if(tp[0]!=0):
+			print(tp)
+			result.append(tp)
+			if(tp[0]=="integer"):
+				ptr+=3
+			else:ptr+=len(tp[1])
+			continue
+		tp=find_op_or_key_words(nexts)
+		if(tp[0]!=0):
+			print(tp)
+			result.append(tp)
+			ptr+=len(tp[0])
+			continue
+		tp=find_number(nexts)
+		if(tp[0]!=0):
+			print(tp)
+			result.append(tp)
+			ptr+=len(tp[1])
+			continue
+		tp=find_identifier(nexts)
+		if(tp[0]!=0):
+			print(tp)
+			result.append(tp)
+			ptr+=len(tp[1])
+			continue
+
+	for i in result:
+		fout.write("{:18}{:}\n".format(i[0],i[1]))
+	fout.flush()
+	fout.close()
+	return result
+
+if __name__ =='__main__':main()
---
+++ 课设/parser.py
@@ -0,0 +1,187 @@
+import sys
+import lex
+
+grammer=list()
+first=dict()
+follow=dict()
+select=dict()
+catergory_dict=set()
+
+def remove_line_break(li:list):
+	for i in range(len(li)):
+		if li[i][-1]=='\n':li[i]=li[i][0:-1]
+
+def read_terminal(file):# 输入终结符
+	global catergory_dict
+	f=open(file,"r",encoding="UTF-8")
+	catergory_dict=f.readlines()
+	remove_line_break(catergory_dict)
+	f.close()
+
+def read_grammer_file(file):# 输入文法
+	global grammer
+	f=open(file,"r",encoding="UTF-8")
+	file_content=f.readlines()
+	f.close()
+	for line in file_content:
+		temp=line.split()# 空格分隔
+		temp.remove("::=")
+		grammer.append(temp)
+	# print(grammer)
+	
+
+def isTerminal(str):return str in catergory_dict
+def isKong(str:str):return str=="eps"
+
+def getFirst():# 课本P78
+	global first
+	first=dict()
+	for line in grammer:
+		for token in line: # 首先加入终结符
+			first[token]=set()
+			if isTerminal(token) or isKong(token):
+				first[token].add(token)
+	for cnt in range(len(grammer)+9):# bellman_ford
+		update=0
+		for line in grammer:
+			lens=len(first[line[0]])
+			addt=line[0] # 产生式行开始的终结符
+			i=1 # 记录该产生式第几个符号
+			for token in line[1:]:
+				i+=1
+				# 产生式无空或者first集有空
+				if ("eps" not in first[token]) or i==len(line):
+					first[addt]=first[addt].union(first[token])
+					break
+
+				t=first[token].copy()
+				t.discard("eps")
+				first[addt]=first[addt].union(t)
+
+			if lens != len(first[addt]): # 有更新
+				update=1
+		if not update:break # 无更新,break
+	# print(first)
+
+
+def getFollow():# 课本P79
+	global follow
+	follow = {}
+	for line in grammer:
+		for token in line:
+			if not isTerminal(token) and token != "eps":
+				follow[token]=set() # 只有非终结符才有follow集
+	follow['program'].add("#")
+
+	while(1):
+		update=0
+		for line in grammer:
+			for i in range(1,len(line)):
+				if isTerminal(line[i]) or line[i] == 'eps':
+					continue
+				lens=len(follow[line[i]])
+				fir=set()
+				for j in range(i+1,len(line)):# 获取后面的first集
+					if ("eps" not in first[line[j]]) or (j==len(line)-1):
+						fir=fir.union(first[line[j]])
+						break
+					t=first[line[j]].copy()
+					t.discard("eps")
+					fir=fir.union(t)
+
+				if ("eps" in fir) or len(fir)==0:
+					fir=fir.union(follow[line[0]])
+					fir.discard("eps") # 去除空
+				follow[line[i]]=follow[line[i]].union(fir)
+				if lens!=len(follow[line[i]]):
+					update=1
+
+		if not update:break
+	# print(follow)
+
+
+def getSelect():
+	global select
+	select =dict()
+	for i in range(len(grammer)):
+		line=grammer[i]
+		j=0
+		fir=set()
+		for j in range(1,len(line)):# 获取产生式后面的first集
+			if ("eps" not in first[line[j]]) or (j==len(line)-1):
+				fir=fir.union(first[line[j]])
+				break
+			t=first[line[j]].copy()
+			t.discard("eps")
+			fir=fir.union(t)
+		if ("eps" in fir):# 如果右边产生式可以为空
+			fir=fir.union(follow[line[0]])# 加入终结符的follow集
+		fir.discard("eps")
+		select[i]=fir
+	# print(select)
+
+terminal=set()
+nterminal=set()
+LL_1Table=dict() # 字典里面的终结符映射到产生式 str -> int
+def getLL_1Table():
+	global terminal,nterminal
+	for line in grammer:
+		for token in line:
+			if isTerminal(token) or token=="eps":
+				terminal.add(token)
+			else: nterminal.add(token)
+	terminal.add("#")
+	for nt in nterminal:
+		LL_1Table[nt]=dict()
+	for line in select:
+		nt=grammer[line][0]
+		for tem in select[line]:
+			if tem not in LL_1Table[nt]:
+				LL_1Table[nt][tem]=line
+			else:
+				print("文法错误")
+				print(LL_1Table[nt])
+	for nt in LL_1Table:
+		print(nt)
+		print(LL_1Table[nt])
+	print("\n\n\n")	
+
+def analysis(file)->bool:
+	stack2=lex.main()
+	stack1=["#","program"]
+	stack2.append(('#',))
+	index=0
+	while 1:
+		tk=stack2[index][0]
+		X=stack1.pop()
+		if isTerminal(X):
+			if tk==X:
+				if X=='#':return 1
+				index+=1
+				print("pop 终结符"+str(X))
+				continue
+			else:return 0
+		else:
+			try:
+				line=grammer[LL_1Table[X][tk]].copy()
+				print("产生式:"+str(line))
+				for i in line[-1:0:-1]:
+					if i!="eps":stack1.append(i)
+			except:return 0
+		print("状态栈")
+		print(stack1[-26:])
+		print("待分析单词")
+		for i in stack2[index:index+26]:
+			print(i[0],end=' ')
+		print('\n\n')
+
+if __name__ == "__main__":
+	read_terminal("terminal.txt")
+	read_grammer_file("grammer.txt")
+	getFirst()
+	getFollow()
+	getSelect()
+	getLL_1Table()
+	if(analysis("Lexical_analysis_result.txt")):
+		print('\n\n分析成功\n\n')
+	else:print("分析失败")
