84cc275f05cac4e8b92f24e0bd821944da168339
1838641320 <1838641320@qq.com>
2022-02-01 21:55:21 +0800

    目标代码生成1

--- 课设/to_assembly.py
+++ 课设/to_assembly.py
@@ -1,9 +1,9 @@
 import hhyu2,re
 
 
-def isid(s:str):
-	res=re.search(r"^[_a-zA-Z][_a-zA-Z0-9]*",s)
-	return res!=None
+def isid(s):
+	res=re.search(r"^[_a-zA-Z][_a-zA-Z0-9]*",str(s))
+	return res!=None and res.group(0)==s
 
 yvyi={
 1:      ['=', '99', '-', 'x'],
@@ -46,50 +46,66 @@ def main():
 	# yvyi=hhyu2.main()
 	# print(yvyi)
 	data_seg=dict()
-	code_set=[]
+	code_seg=[]
 	def get_v(s:str,regx:str='ax'):
-		nonlocal code_set
-		if(s in data_seg):code_set+=['\tmov {},word ptr [{}]'.format(regx,s)]
-		else:			code_set+=['\tmov {},{}'.format(regx,int(float(s)))]
+		nonlocal code_seg
+		if(s in data_seg):code_seg+=['\tmov {},word ptr [{}]'.format(regx,s)]
+		else:
+			try:code_seg+=['\tmov {},{}'.format(regx,int(float(s)))]
+			except:pass # string assignment
 		
 	for _ in yvyi:# 变量定义
 		line=yvyi[_]
-		for tk in line:
+		for _ in range(1,4):
+			tk=line[_]
 			if(isid(tk) and tk not in data_seg):
 				if(line[0]=='[]='):# 数组
 					if(line[2].isnumeric()):
-						data_seg[tk]="dw "+line[2]+"dup(0)"
+						data_seg[tk]="dw {} dup(0)".format(line[2])
 					else:
-						data_seg[tk]="db "+line[1]+",0"
+						data_seg[tk]="db {},0".format(line[1])
 					continue
 				data_seg[tk]="dw 0"
 	
-	for _ in yvyi:
+	for _ in yvyi:# 代码生成
 		line=yvyi[_]
-		code_set+=["line__{}:".format(_)]
-		if(line[0][0]=='j'):# 跳转
+		if(_>1 and code_seg[-1][-1]==':'):
+			code_seg.pop(-1)
+		code_seg+=["line{}:".format(_)]
+		if(line[0]=='[]='):
+			pass
+		elif(line[0][0]=='j'):# 跳转
 			jmp_dic={'j>':'jg','j=':'je','j<':'jl','j':'jmp'}
 			if(line[0]!='j'):
 				get_v(line[1],'ax')
 				get_v(line[2],'bx')
-				code_set+=['\tcmp ax,bx']
-			code_set+=['\t{} line__{}'.format(jmp_dic[line[0]],line[3])]
+				code_seg+=['\tcmp ax,bx']
+			code_seg+=['\t{} line{}'.format(jmp_dic[line[0]],line[3])]
 			
 		elif(line[0]=='='):# 赋值 
 			get_v(line[1])
-			code_set+=['\tmov word ptr[{}],ax'.format(line[3])]
+			code_seg+=['\tmov word ptr[{}],ax'.format(line[3])]
 		else:
 			# operator
-			if(line[0]=='+'):
-				get_v(line[1],'ax')
-				get_v(line[2],'bx')
-				code_set+=['\tadd ax,bx']
-			
-
-
-			code_set+=['\tmov word ptr']
+			get_v(line[1],'ax')
+			get_v(line[2],'bx')
+			if(line[0]=='+'):code_seg+=['\tadd ax,bx']
+			if(line[0]=='-'):code_seg+=['\tsub ax,bx']
+			if(line[0]=='*'):code_seg+=['\timul ax,bx']
+			if(line[0]=='/'):
+				code_seg+=['\txor dx,dx']
+				code_seg+=['\tidiv bx']
+			code_seg+=['\tmov word ptr[{}],ax'.format(line[3])]
 	
-
+	# 头尾工作
+	result=[".model small\ninclude io.inc\n.686P\n.stack 200h\n.data"]
+	for _ in data_seg:result+=["\t{} ".format(_)+data_seg[_]]
+	result+=['.code\n.startup']
+	for _ in code_seg:result+=[_]
+	result+=['.exit\nend']
+	f=open("final.asm",'w',encoding='UTF-8')
+	for line in result:f.write(line+"\n")
+	f.flush()
 
 if __name__ == "__main__":
 	main()
